bea73e83f0d9b0728a48df96db17b14a91ee2c69
Vitor Rangel <1620295+vrangel@users.noreply.github.com>
2019-01-11 17:12:21 -0200

    Update project-metric.py

--- project-metric.py
+++ project-metric.py
@@ -1,17 +1,28 @@
 #!/usr/bin/env python3
 import sys, requests
-token, url = sys.argv[1], sys.argv[2]
+usage = 'Usage: projects.py [ <user> <password> | <token> ] <url>'
+if  len(sys.argv) == 2 and sys.argv[1] in ['-h', '-H', '--help']:
+    sys.exit(usage)
+elif len(sys.argv) == 3: # admin token and sonarqube url
+    user_token, password, url = sys.argv[1], '', sys.argv[2]
+elif len(sys.argv) == 4: # admin user, password and sonarqube url
+    user_token, password, url = sys.argv[1], sys.argv[2], sys.argv[3]
+else:
+    sys.exit('Syntax error. ' + usage)
+if url.endswith('/'):
+    url = url[:-1]
 payload = {'ps': 500}
-projects = requests.get(url + '/api/projects/search', params=payload, auth=(token, ''))
-metrics = 5
+projects = requests.get(url + '/api/projects/search', params=payload, auth=(user_token, password))
 for project in projects.json()['components']:
-    print(project['name'] + ',', end='')
-    payload = {'component': project['key'], 'metricKeys': 'bugs,code_smells,coverage,duplicated_lines_density,vulnerabilities'}
-    measures = requests.get(url + '/api/measures/component', params=payload)
-    for index in range(0, metrics):
-        measure = measures.json()['component']['measures']
-        try:
-            print(measure[index]['metric'] + ':' + measure[index]['value'] + ',', end='')
-        except IndexError:
-            pass
+    payload = {'component': project['key'], 'metricKeys': 'bugs,code_smells,coverage,duplicated_lines_density,vulnerabilities,ncloc,sqale_index,security_remediation_effort,reliability_remediation_effort'}
+    metrics = requests.get(url + '/api/measures/component', params=payload).json()['component']['measures']
+    if len(metrics) > 0:
+        print(project['name'] + ',', end='')
+    else:
+        print(project['name'], end='')
+    for index in range(0, len(metrics)):
+        if index == len(metrics) -1:
+            print(metrics[index]['metric'] + ':' + metrics[index]['value'], end='')
+        else:
+            print(metrics[index]['metric'] + ':' + metrics[index]['value'] + ',', end='')
     print()
